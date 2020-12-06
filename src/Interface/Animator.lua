703c0626dc5d01dae55e2ce8baeee939bba96cdc
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-06 12:16:56 -0500

    started Component & Animator libs

---
+++ src/Interface/Animator.lua
@@ -0,0 +1,199 @@
+--[=[
+	@Author: Gavin "Mullets" Rosenthal
+	@Desc: An Animation library for GuiObjects [WIP]
+]=]
+
+--[=[
+[DOCUMENTATION]:
+	Simply use Tiffany's tag editor & apply the tags listed below
+	to GuiObjects which take the given events! Settings are attributes
+	to the script.
+	
+	Tag editor:
+	https://www.roblox.com/library/948084095/Tag-Editor
+
+[TAGS]:
+	MouseButton1Click:
+		UI_BounceUp
+		UI_BounceDown
+		
+	MouseButton1Down:
+		
+	MouseButton1Up:
+		
+	MouseEnter:
+		UI_Grow
+		
+	MouseLeave:
+		UI_Reset
+]=]
+
+local Animator = {}
+Animator.Cache = {}
+Animator.Original = {}
+Animator.Prefix = 'UI_'
+Animator.TagList = {
+	['MouseButton1Click'] = {'BounceUp','BounceDown'};
+	['MouseButton1Down'] = {};
+	['MouseButton1Up'] = {};
+	['MouseEnter'] = {'Grow',''};
+	['MouseLeave'] = {'Reset'};
+}
+
+local Loader = require(game:GetService('ReplicatedStorage'):WaitForChild('Loader'))
+local Manager = Loader('Manager')
+local HttpService = Loader['HttpService']
+local CollectionService = Loader['CollectionService']
+
+local AttributeFlag = pcall(function() -- TODO: remove this when Attributes are released
+	script:GetAttribute('Test')
+end)
+
+local Length = AttributeFlag and script:GetAttribute('AnimationTime') or 0.1
+local Scale = AttributeFlag and script:GetAttribute('AnimationScale') or 0.1
+
+--------------------
+-- Common Effects --
+--------------------
+Animator['FadeOut'] = function(element)
+	
+end
+
+Animator['FadeAll'] = function(container)
+	
+end
+
+-----------------------
+-- MouseButton1Click --
+-----------------------
+Animator['BounceUp'] = function(element)
+	local original = Animator.Original[element]
+	
+	local increment = 1 - Scale
+	local goal; do
+		if original.Position.X.Scale > 0 and original.Position.X.Offset == 0 then
+			goal = UDim2.fromScale(original.Position.X.Scale,original.Position.Y.Scale * increment)
+		elseif original.Position.X.Offset > 0 and original.Position.X.Scale == 0 then
+			goal = UDim2.fromOffset(original.Position.X.Offset,original.Position.Y.Offset * increment)
+		end
+	end
+	
+	local tween = Manager.Tween(element,{'Position'},{goal},Length)
+	tween.Completed:Wait()
+	tween = Manager.Tween(element,{'Position'},{original.Position},Length)
+	tween.Completed:Wait()
+end
+
+Animator['BounceDown'] = function(element)
+	local original = Animator.Original[element]
+	
+	local increment = Scale + 1
+	local goal; do
+		if original.Position.X.Scale > 0 and original.Position.X.Offset == 0 then
+			goal = UDim2.fromScale(original.Position.X.Scale,original.Position.Y.Scale * increment)
+		elseif original.Position.X.Offset > 0 and original.Position.X.Scale == 0 then
+			goal = UDim2.fromOffset(original.Position.X.Offset,original.Position.Y.Offset * increment)
+		end
+	end
+	
+	local tween = Manager.Tween(element,{'Position'},{goal},Length)
+	tween.Completed:Wait()
+	tween = Manager.Tween(element,{'Position'},{original.Position},Length)
+	tween.Completed:Wait()
+end
+
+----------------
+-- MouseEnter --
+----------------
+Animator['Grow'] = function(element)
+	local original = Animator.Original[element]
+	
+	local increment = Scale + 1
+	local goal; do
+		if original.Size.X.Scale > 0 and original.Size.X.Offset == 0 then
+			goal = UDim2.fromScale(original.Size.X.Scale * increment,original.Size.Y.Scale * increment)
+		elseif original.Size.X.Offset > 0 and original.Size.X.Scale == 0 then
+			goal = UDim2.fromOffset(original.Size.X.Offset * increment,original.Size.Y.Offset * increment)
+		end
+	end
+	
+	local tween = Manager.Tween(element,{'Size'},{goal},Length)
+	tween.Completed:Wait()
+end
+
+----------------
+-- MouseLeave --
+----------------
+Animator['Reset'] = function(element)
+	local original = Animator.Original[element]
+	
+	local prop,value = {},{}
+	for index,base in pairs(original) do
+		table.insert(prop,index)
+		table.insert(value,base)
+	end
+	
+	local tween = Manager.Tween(element,prop,value,Length)
+	tween.Completed:Wait()
+end
+
+-----------------
+-- Connections --
+-----------------
+local function ConnectAnimation(element,event,tag)
+	local identifier = event..'_'..tag..'_'..tostring(element)
+	if Animator.Cache[element] and Animator.Cache[element][identifier] then return end
+	
+	local IsImage = element:IsA('ImageButton') or element:IsA('ImageLabel')
+	
+	if not Animator.Original[element] then
+		Animator.Original[element] = {
+			AnchorPoint = element.AnchorPoint;
+			Position = element.Position;
+			Size = element.Size;
+			
+			BackgroundColor3 = element.BackgroundColor3;
+			BackgroundTransparency = element.BackgroundTransparency;
+			BorderColor3 = element.BorderColor3;
+			
+			ImageColor3 = IsImage and element.ImageColor3 or nil;
+			ImageTransparency = IsImage and element.ImageTransparency or nil;
+		}
+	end
+	
+	local guid = HttpService:GenerateGUID(false)
+	local code = Animator[string.sub(tag,#Animator.Prefix + 1)]
+	local signal = element[event]:Connect(function(...)
+		local data = {...}
+		
+		Manager.debounce(guid,function()
+			code(element,table.unpack(data))
+		end)
+	end)
+	
+	if not Animator.Cache[element] then
+		Animator.Cache[element] = {}
+	end
+	
+	Animator.Cache[element][identifier] = signal
+end
+
+local function ConnectTag(event,tag)
+	CollectionService:GetInstanceAddedSignal(tag):Connect(function(element)
+		ConnectAnimation(element,event,tag)
+	end)
+	
+	local Tagged = CollectionService:GetTagged(tag)
+	for index,element in pairs(Tagged) do
+		ConnectAnimation(element,event,tag)
+	end
+end
+
+for event,list in pairs(Animator.TagList) do
+	for count,tag in pairs(list) do
+		tag = Animator.Prefix..tag
+		ConnectTag(event,tag)
+	end
+end
+
+return Animator
---
+++ src/Interface/Components.lua
@@ -0,0 +1,246 @@
+--[=[
+	@Author: Gavin "Mullets" Rosenthal
+	@Desc: Internal Modular Component System
+]=]
+
+local Components = {}
+Components._Name = 'Modular Component System'
+Components._Error = '[MCS]: '
+Components._Bindings = {}
+
+local Loader = require(game:GetService('ReplicatedStorage'):WaitForChild('Loader'))
+local Manager = Loader('Manager')
+local RunService = Loader['RunService']
+
+--[=[
+	Construct a new component out of a pre-existing element
+	
+	@param element GuiObject 
+]=]
+function Components.new(element: GuiObject)
+	local config = element:FindFirstChildWhichIsA('Configuration') do
+		if not config then
+			config = Instance.new('Configuration')
+			config.Name = 'MCS_'..element.Name
+			config.Parent = element
+		end
+	end
+	
+	return setmetatable({
+		element = element;
+		config = config;
+	},Components)
+end
+
+--[=[
+	Bind a function to a codename
+	
+	@param name string -- the name of the binding
+	@param code function -- the function to bind
+	@return nil
+]=]
+function Components:Bind(name: string, code: (any) -> nil): nil
+	assert(Components._Bindings[name] == nil,"Attempted to overwrite binding on '"..name.."'")
+	
+	Components._Bindings[name] = code
+end
+
+--[=[
+	Unbind a codename
+	
+	@param name string -- the name of the binding
+	@return nil
+]=]
+function Components:Unbind(name: string): nil
+	Components._Bindings[name] = nil
+end
+
+--[=[
+	Fire a binded function on a codename
+	
+	@param name string -- the name of the binding
+	@param ...? any -- optional parameters to pass
+	@return nil
+]=]
+function Components:Fire(name: string, ...): nil
+	assert(Components._Bindings[name],Components._Error.."Attempted to fire a non-existant binding on '"..name.."'")
+	
+	Manager.wrap(Components._Bindings[name],...)
+end
+
+--[=[
+	Get an attribute value on the component. Checks for value objects first
+	
+	@param name string -- name of the attribute
+	@return Value any
+]=]
+function Components:Get(name: string): any
+	local obj = self.config:FindFirstChild(name)
+	
+	if obj then
+		return obj.Value
+	else
+		return self.config:GetAttribute(name)
+	end
+end
+
+--[=[
+	Set an attribute value on the component. Checks for value objects first
+	
+	@param name string -- name of the attribute
+	@param value any -- the value to set on an attribute
+	@return Value any
+]=]
+function Components:Set(name: string, value: any)
+	local obj = self.config:FindFirstChild(name)
+	
+	if obj then
+		obj.Value = value
+	else
+		self.config:SetAttribute(name,value)
+	end
+	
+	return value
+end
+
+--[=[
+	Update a known attribute with a value & increment numbers
+	
+	@param name string -- the name of the attribute
+	@param value any -- the value to update on an attribute
+	@return Value any
+]=]
+function Components:Update(name: string, value: any)
+	local get = self:Get(name)
+	
+	assert(get ~= nil,Components._Error.."Attempted to update nil attribute '"..name.."'")
+	
+	if typeof(get) == 'number' and typeof(value) == 'number' then
+		get += value
+		self:Set(name,get)
+	else
+		self:Set(name,value)
+	end
+	
+	return self:Get(name)
+end
+
+--[=[
+	Bind a function to an attribute that changes. Checks for value objects first
+	
+	@param name string -- the name of the attribute
+	@param code function -- the function to connect
+	@return RBXScriptSignal
+]=]
+function Components:Attribute(name: string, code: (any, any) -> nil): RBXScriptSignal
+	local last = self:Get(name)
+	
+	assert(last ~= nil,Components._Error.."Attempted to bind to nil attribute '"..name.."'")
+	
+	Manager.wrap(code,last,last)
+	
+	local obj = self.config:FindFirstChild(name)
+	local signal; do
+		if obj then
+			signal = obj.Changed:Connect(function(new)
+				Manager.wrap(code,new,last)
+				
+				last = new
+			end)
+		else
+			signal = self.config:GetAttributeChangedSignal(name):Connect(function()
+				local new = self:Get(name)
+				
+				Manager.wrap(code,new,last)
+				
+				last = new
+			end)
+		end
+	end
+	
+	Manager:ConnectKey('attribute_'..name,signal)
+	
+	return signal
+end
+
+--[=[
+	Connect an event to a GuiObject apart of the component
+	
+	@param object GuiObject -- the object to connect
+	@param event string -- the connection type
+	@param code function -- the function to connect
+	@return RBXScriptSignal
+]=]
+function Components:Connect(object: GuiObject, event: string, code: (any) -> nil): RBXScriptSignal
+	local signal = object[event]:Connect(function(...)
+		code(...)
+	end)
+	
+	Manager:ConnectKey('connection_'..object.Name,signal)
+	
+	return signal
+end
+
+--[=[
+	Hook a function to a lifecycle event which fires when the component is visible
+	
+	@param name string -- the name of the lifecycle
+	@param code function -- the function to run
+	@return RBXScriptSignal
+]=]
+function Components:Lifecycle(name: string, code: (number) -> nil)
+	local signal = RunService.RenderStepped:Connect(function(delta)
+		if self.element.Visible then
+			code(delta)
+		end
+	end)
+	
+	Manager:ConnectKey('lifecycle_'..name,signal)
+	
+	return signal
+end
+
+--[=[
+	Destroys all the signals connected to a name no matter the type
+	
+	@param name string -- name of the signal key connected
+]=]
+function Components:Destroy(name: string)
+	Manager:DisconnectKey('attribute_'..name)
+	Manager:DisconnectKey('connection_'..name)
+	Manager:DisconnectKey('lifecycle_'..name)
+end
+
+--[=[
+	A custom index method which handles unknown or known indices
+	
+	@param index any -- the index being called on the component
+	@return any?
+]=]
+function Components:__index(index: any)
+	if Components[index] then
+		return Components[index]
+	end
+	
+	if index == self.element.Name then
+		return self.element
+	end
+	
+	if self.element[index] then
+		return self.element[index]
+	end
+	
+	error(Components._Error..index..' is not a valid member of '..self.element:GetFullName()..' "'..self.element.ClassName..'"',2)
+end
+
+--[=[
+	Shorten getting an attribute attached to the component
+	
+	@param name string -- name of the component
+	@return Value any
+]=]
+function Components:__call(name: string)
+	return self:Get(name)
+end
+
+return Components
